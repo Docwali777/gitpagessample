2e0a22c40c672b46758245f26f8d5d76426bd704
docwali777 <drgauvin@mgmcare.org>
2017-01-01 03:03:52 -0500

    Dry Run

--- basic.js
+++ basic.js
@@ -1,14 +1,25 @@
 var express = require("express");
+var path = require("path")
+var hbs = require("hbs")
+
 var app = express();
+
 app.set("port", process.env.PORT || 3000);
 
-app.use(express.static(__dirname + "/public"))
+app.set(path.join(__dirname + "/public"))
+app.set("view engine", "hbs")
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+//app.use(express.static(__dirname + "/public"))
 app.get("/", (req, res)=>{
-  res.render("home")
+  res.render("index")
 })
 
-app.get("/about.html", (req, res)=>{
-  res.render("about")
+app.get("/about", (req, res)=>{
+  res.render("about", {
+    name: " Wali Gauvin"
+  })
 })
 
 app.listen(app.get("port"))
